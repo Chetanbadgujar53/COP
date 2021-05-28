a7f6af91caba2e67beeff23b324ce1b557daa74b
chetan <chetanbadgujar003@gmail.com>
2021-05-28 16:57:29 +0530

    new updating filr prgram for sample porpose

---
+++ demo/programs/program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    int add;
+    printf("Enter the the two numbers :");
+    scanf("%d%d",&a,&b);
+    add=a+b;
+    printf("Addition of two numberd :%d",add);
+    
+    return 0;
+}
